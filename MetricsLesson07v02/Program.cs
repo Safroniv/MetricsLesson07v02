a1c53fa0857a34108b7b39490be6ab3db1f0d456
Иван Сафронов <safroniv@mail.ru>
2023-05-25 00:01:26 +0300

    Add project files.

---
+++ MetricsLesson07v02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MetricsLesson07v02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
